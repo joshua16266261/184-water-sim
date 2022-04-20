07ab7357f51d427496e829d6259ce6c2dbdaf5cf
Joshua Chen <joshua6261@att.net>
2022-04-19 22:36:59 -0700

    Added write_pos_to_file

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,19 @@
 #include "particle.h"
 #include <vector>
 
+#include <fstream>
+
 using namespace std;
+
+void write_pos_to_file(Fluid *f, string filename) {
+	string s = "";
+	for (auto p = begin(f->particles); p != end(f->particles); p++) {
+		s.append("(" + to_string(p->position[0]) + "," + to_string(p->position[1]) + "," + to_string(p->position[2]) + ")" + '\n');
+	}
+	ofstream file(filename);
+	file << s;
+	file.close();
+}
 
 int main(int argc, char **argv) {
 	Fluid *f = new Fluid(5, 5, 5, 5, 5, 5);
@@ -31,6 +43,8 @@ int main(int argc, char **argv) {
 	vector<Vector3D> accel = vector<Vector3D>{g};
 	
 	f->simulate(fp, accel, nullptr);
+	
+	write_pos_to_file(f, "output.txt");
 	
 	for (auto p = begin(f->particles); p != end(f->particles); p++) {
 		std::cout << p->position << '\n';
