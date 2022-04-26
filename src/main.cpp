7cbd94b47315ae3f3eff24be79bc92aa2e76f8a8
Joshua Chen <joshua6261@att.net>
2022-04-26 00:32:29 -0700

    Added command line args for parameter testing

--- src/main.cpp
+++ src/main.cpp
@@ -30,22 +30,14 @@ int main(int argc, char **argv) {
 //	fp->vorticity_eps = 0.005;
 //	fp->relaxation = 5000;
 	
-	
 	Fluid *f = new Fluid(4, 4, 4, 40, 40, 40);
-	
+
 	#pragma omp parallel for
 	for (auto p = begin(f->particles); p != end(f->particles); p++) {
 		p->position += Vector3D(0, 0, 4);
 	}
-
+	
 	FluidParameters *fp = new FluidParameters(EPS_F, 3, 60, 5);
-	fp->h = 0.15;
-	fp->delta_q = 0.02 * fp->h * Vector3D(1, 1, 1);
-	fp->density = 1000;
-	fp->k = 0.001;
-	fp->c = 0.0005;
-	fp->vorticity_eps = 0.0005;
-	fp->relaxation = 5000;
 	
 	Vector3D g = Vector3D(0, 0, -9.81);
 	vector<Vector3D> accel = vector<Vector3D>{g};
@@ -57,6 +49,46 @@ int main(int argc, char **argv) {
 	Plane *back_wall = new Plane(Vector3D(0, 6, 0), Vector3D(0, -1, 0), 0);
 	vector<CollisionObject *> collision = vector<CollisionObject *>{floor, left_wall, right_wall, front_wall, back_wall};
 	
+	const string h = "--h";
+	const string delta_q = "--q";
+	const string k = "--k";
+	const string c = "--c";
+	const string vort = "--vort";
+	const string relax = "--relax";
+	const string time = "--time";
+	const string fps = "--fps";
+	
+	for (int i = 1; i < argc - 1; i++) {
+		std::cout << argv[i] << '\n';
+		if (argv[i] == h) {
+			fp->h = stod(argv[i + 1]);
+		} else if (argv[i] == delta_q) {
+			// --q param must come after --h param
+			fp->delta_q = stod(argv[i + 1]) * fp->h * Vector3D(1, 0, 0);
+		} else if (argv[i] == k) {
+			fp->k = stod(argv[i + 1]);
+		} else if (argv[i] == c) {
+			fp->c = stod(argv[i + 1]);
+		} else if (argv[i] == vort) {
+			fp->vorticity_eps = stod(argv[i + 1]);
+		} else if (argv[i] == relax) {
+			fp->relaxation = stod(argv[i + 1]);
+		} else if (argv[i] == time) {
+			fp->total_time = stod(argv[i + 1]);
+		} else if (argv[i] == fps) {
+			fp->fps = stod(argv[i + 1]);
+		}
+	}
+	
+//	fp->h = 0.15;
+//	fp->delta_q = 0.1 * fp->h * Vector3D(1, 0, 0);
+	fp->density = 1000;
+//	fp->k = 0.001;
+////	fp->c = 0.0005;
+//	fp->c = 0.00075;
+//	fp->vorticity_eps = 0.0005;
+//	fp->relaxation = 4000;
+
 	for (int frame = 0; frame < fp->total_time * fp->fps; frame++) {
 		std::cout << frame << '\n';
 		f->simulate(fp, accel, &collision);
